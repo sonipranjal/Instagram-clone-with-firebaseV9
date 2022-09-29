7890894f9c2d6188c032a65bfdb2be9767a69860
sonipranjal <pranjalsoni6256@gmail.com>
2022-09-30 00:00:17 +0530

    finished all parts

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,11 +1,113 @@
+import { uuidv4 } from '@firebase/util';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
 import Image from 'next/image';
-import React from 'react';
-import { AiOutlineHeart } from 'react-icons/ai';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import { BsBookmark, BsEmojiSmile, BsThreeDots } from 'react-icons/bs';
 import { FaRegComment } from 'react-icons/fa';
 import { IoShareOutline } from 'react-icons/io5';
+import { auth, db } from '../../lib/firebase';
+import { GlobalContext } from '../../state/context/GlobalContext';
 
-const Post = ({ id, username, image, caption }) => {
+const Post = ({ id, username, image, caption, likesCount }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [comments, setComments] = useState([]);
+
+  const handleLikePost = async () => {
+    const postLike = {
+      postId: id,
+      userId: auth.currentUser.uid,
+      username,
+    };
+
+    const likeRef = doc(db, `likes/${id}_${auth.currentUser.uid}`);
+    const postRef = doc(db, `posts/${id}`);
+
+    let updatedLikesCount;
+
+    if (isLiked) {
+      await deleteDoc(likeRef);
+      if (likesCount) {
+        updatedLikesCount = likesCount - 1;
+      } else {
+        updatedLikesCount = 0;
+      }
+      await updateDoc(postRef, {
+        likesCount: updatedLikesCount,
+      });
+    } else {
+      await setDoc(likeRef, postLike);
+      if (likesCount) {
+        updatedLikesCount = likesCount + 1;
+      } else {
+        updatedLikesCount = 1;
+      }
+      await updateDoc(postRef, {
+        likesCount: updatedLikesCount,
+      });
+    }
+  };
+
+  useEffect(() => {
+    const likesRef = collection(db, 'likes');
+    const likesQuery = query(
+      likesRef,
+      where('postId', '==', id),
+      where('userId', '==', auth.currentUser.uid)
+    );
+
+    const unsubscribeLike = onSnapshot(likesQuery, (snapshot) => {
+      const like = snapshot.docs.map((doc) => doc.data());
+      if (like.length !== 0) {
+        setIsLiked(true);
+      } else {
+        setIsLiked(false);
+      }
+    });
+
+    const commentsRef = collection(db, `posts/${id}/comments`);
+    const commentsQuery = query(commentsRef, orderBy('createdAt', 'desc'));
+
+    const unsubscribeComments = onSnapshot(commentsQuery, (snapshot) => {
+      const comments = snapshot.docs.map((doc) => doc.data());
+      setComments(comments);
+    });
+
+    return () => {
+      unsubscribeLike();
+      unsubscribeComments();
+    };
+  }, [id]);
+
+  const comment = useRef(null);
+
+  const { user } = useContext(GlobalContext);
+
+  const handlePostComment = async (e) => {
+    e.preventDefault();
+    // comment functionality
+    const commentData = {
+      id: uuidv4(),
+      username: user.username,
+      comment: comment.current.value,
+      createdAt: serverTimestamp(),
+    };
+    comment.current.value = '';
+    const commentRef = doc(db, `posts/${id}/comments/${commentData.id}`);
+    await setDoc(commentRef, commentData);
+  };
+
   return (
     <div className="flex flex-col w-full border border-gray-200">
       <div className="flex items-center justify-between w-full p-2 ">
@@ -27,11 +129,18 @@ const Post = ({ id, username, image, caption }) => {
       </div>
       <div className="flex justify-between p-2 text-lg">
         <div className="flex space-x-2">
-          <div>
-            <AiOutlineHeart
-              size={25}
-              className="text-black cursor-pointer hover:text-black/50"
-            />
+          <div onClick={handleLikePost}>
+            {isLiked ? (
+              <AiFillHeart
+                size={25}
+                className="text-red-500 cursor-pointer hover:text-red-500/50"
+              />
+            ) : (
+              <AiOutlineHeart
+                size={25}
+                className="text-black cursor-pointer hover:text-black/50"
+              />
+            )}
           </div>
           <div>
             <FaRegComment
@@ -53,14 +162,16 @@ const Post = ({ id, username, image, caption }) => {
           />
         </div>
       </div>
-      <div className="px-2">1000 likes</div>
+      <div className="px-2">
+        {likesCount ? `${likesCount} likes` : 'Be the first to like'}
+      </div>
       <div className="px-2">{caption}</div>
       <div className="p-2">
         <div className="flex flex-col space-y-1">
-          {new Array(3).fill(0).map((_, i) => (
-            <div key={i} className="flex space-x-2">
-              <div className="font-medium">username</div>
-              <div>comment {i + 1}</div>
+          {comments.map((commentData) => (
+            <div key={commentData.id} className="flex space-x-2">
+              <div className="font-medium">{commentData.username}</div>
+              <div>{commentData.comment}</div>
             </div>
           ))}
         </div>
@@ -70,7 +181,7 @@ const Post = ({ id, username, image, caption }) => {
         <div>
           <BsEmojiSmile className="text-xl" />
         </div>
-        <form onSubmit={(e) => e.preventDefault()} className="flex w-full px-2">
+        <form onSubmit={handlePostComment} className="flex w-full px-2">
           <div className="w-full">
             <input
               type="text"
@@ -78,6 +189,7 @@ const Post = ({ id, username, image, caption }) => {
               id={`comment ${id}`}
               className="w-full bg-white outline-none"
               placeholder="Add a comment..."
+              ref={comment}
             />
           </div>
           <div>
